df81f1ab550a911a89cd960ccca86c46aaa22516
Z-Sir001 <979190403@qq.com>
2023-09-07 13:36:53 +0800

    mi

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/docs_books/",
   title: "Mr.Z",
   description: "A VitePress Site",
   themeConfig: {
